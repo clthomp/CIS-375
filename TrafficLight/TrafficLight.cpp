2316ab4e3795aeb43746cb8f4db7da4c56b02165
Nuthen <nuthen@comcast.net>
2017-12-01 00:16:12 -0500

    added SETTINGS class for parameters. changed densities to float. improved some comments

--- TrafficLight/TrafficLight.cpp
+++ TrafficLight/TrafficLight.cpp
@@ -2,50 +2,69 @@
 #include <vector>
 #include <queue>
 #include <string>
+#include <algorithm> 
 
 using namespace std;
 
+bool DEBUG_MODE = true;
+
+struct Settings {
+	int minimumLightTime = 15;
+	int maximumLightTime = 60;
+
+	// time it takes before cars begin moving after a light turns green
+	float carStartupTime = 3;
+	// average number of cars that can pass through a light per 1 second after cars have sped up
+	float carPassingRate = 1.0;
+	// how many cars per 1 unit of density
+	float densityToCarRatio = 1.0;
+	// the importance travel time has on shifting density
+	float travelTimeDensityMultiplier = 1.0;
+
+	// how much to try moving each light time up or down at each iteration
+	int lightTimeTestValueChange = 1;
+};
+Settings SETTINGS;
+
 class Intersection;
 
+// class written by Robert Piepsney
 class Road {
 private:
-	//string name;
-	int travelTime;
-	//vector<Intersection> endPoints;
-	void setEndPoint(Intersection point); // located following Intersection class definition
+
 public:
 	string name;
 	string id;
+	float travelTime;
+	int mainLanes;
 	vector<Intersection> endPoints;
-	Road(string name, string id, int travelTime) {
+	Road(string name, string id, float travelTime, int mainLanes) {
 		this->name = name;
 		this->id = id;
-		travelTime = travelTime;
+		this->travelTime = travelTime;
+		this->mainLanes = mainLanes;
 	}
+	void setEndPoint(Intersection point); // located following Intersection class definition
 };
 
 // class written by Robert Piepsney
 class Intersection {
 private:
-	// assume roads with the same name lead the same way
-	//vector<Road*> roadsThatIntersect;
-	//string name;
-	//vector<int> roadDensity;
+
 public:
 	vector<Road*> roadsThatIntersect;
 	string name;
-	vector<int> roadDensity; // parallel to roadsThatIntersect
+	vector<float> roadDensity; // parallel to roadsThatIntersect
 	struct TogetherRoad {
 		vector<Road*> roads;
 		string name;
-		//int time;
 		TogetherRoad(Road* initialRoad) {
 			roads.push_back(initialRoad);
 			this->name = initialRoad->name;
 		}
 	};
 	vector<TogetherRoad> togetherRoads;
-	Intersection(string name, vector<Road*>roads, vector<int>density) {
+	Intersection(string name, vector<Road*>roads, vector<float>density) {
 		this->name = name;
 		roadsThatIntersect = roads;
 		roadDensity = density;
@@ -79,6 +98,12 @@ public:
 				}
 			}
 		}
+
+		if (DEBUG_MODE) {
+			cout << "Error: No road to light index found" << endl;
+		}
+
+		return 0;
 	}
 };
 
@@ -86,18 +111,16 @@ void Road::setEndPoint(Intersection point) {
 	endPoints.push_back(point);
 }
 
-int getCarsPassing(int t) {
-	return t;
+float getCarsPassing(float t) {
+	t -= SETTINGS.carStartupTime;
+	t *= SETTINGS.carPassingRate;
+	return max(float(0.0), t);
 }
 
 // class written by Robert Piepsney
 class Map {
 private:
 	vector<Road> roads;
-	//vector<Intersection> intersections;
-
-	int minimumLightTime;
-	int maximumLightTime;
 public:
 	vector<Intersection> intersections;
 	void importRoads(Road inputRoads) {
@@ -106,26 +129,20 @@ public:
 	void importIntersections(Intersection inputIntersections) {
 		intersections.push_back(inputIntersections);
 	}
-	void setMin(int min) {
-		minimumLightTime = min;
-	}
-	void setMax(int max) {
-		maximumLightTime = max;
-	}
 
 	vector<vector<int>> tempSolutions;
 
 	// recursive method to generate all possible changes for light times
-	// a light time can either increase, decrease, or stay the same
+	// a light time can either increase, decrease, or stay the same by the value given from SETTINGS
 	void getLightChanges(vector<int> currentChanges, int current) {
 		if (current >= currentChanges.size()) {
 			tempSolutions.push_back(currentChanges);
 		}
 		else {
 			getLightChanges(currentChanges, current + 1);
-			currentChanges[current] += 1;
+			currentChanges[current] += SETTINGS.lightTimeTestValueChange;
 			getLightChanges(currentChanges, current + 1);
-			currentChanges[current] -= 2;
+			currentChanges[current] -= SETTINGS.lightTimeTestValueChange * 2;
 			getLightChanges(currentChanges, current + 1);
 		}
 	}
@@ -137,85 +154,93 @@ public:
 				// for the density of each incoming road
 				// add density for all incoming traffic
 				// remove density for all outgoing traffic
+				Road* thisRoad = intersections[i].roadsThatIntersect[j];
 
 				// outgoing traffic
 				// find time for a road's light
 				int lightIndex = intersections[i].roadIndexToLight(j);
 				int lightTime = currentLightTimes[i][lightIndex];
-				intersections[i].roadDensity[j] -= getCarsPassing(lightTime);
+				intersections[i].roadDensity[j] -= getCarsPassing(lightTime) * SETTINGS.densityToCarRatio *
+													float(thisRoad->mainLanes) /
+													(thisRoad->travelTime * SETTINGS.travelTimeDensityMultiplier);
 
 				// incoming traffic
 				// the density leaving from the neighbor in that direction, only from the same direction
 				// assumption that turns will even out
-				Road* thisRoad = intersections[i].roadsThatIntersect[j];
-				Intersection* neighborIntersection;
+				bool neighborIntersectionFound = false;
+				Intersection* neighborIntersection = &thisRoad->endPoints[0];
 				for (int k = 0; k < thisRoad->endPoints.size(); k++) {
 					if (&thisRoad->endPoints[k] != &intersections[i]) {
 						neighborIntersection = &thisRoad->endPoints[k];
 					}
 				}
 
-				int neighborIntersectionIndex;
-				for (int k = 0; k < intersections.size(); k++) {
-					if (neighborIntersection == &intersections[k]) {
-						neighborIntersectionIndex = k;
+				if (neighborIntersectionFound) {
+					int neighborIntersectionIndex = 0;
+					for (int k = 0; k < intersections.size(); k++) {
+						if (neighborIntersection == &intersections[k]) {
+							neighborIntersectionFound = true;
+							neighborIntersectionIndex = k;
+						}
 					}
-				}
 
-				int neighborParallelRoadIndex;
-				for (int k = 0; k < neighborIntersection->roadsThatIntersect.size(); k++) {
-					if (neighborIntersection->roadsThatIntersect[k]->name == thisRoad->name
-						&& neighborIntersection->roadsThatIntersect[k]->id != thisRoad->id) {
+					int neighborParallelRoadIndex;
+					for (int k = 0; k < neighborIntersection->roadsThatIntersect.size(); k++) {
+						if (neighborIntersection->roadsThatIntersect[k]->name == thisRoad->name
+							&& neighborIntersection->roadsThatIntersect[k]->id != thisRoad->id) {
 
-						neighborParallelRoadIndex = k;
+							neighborParallelRoadIndex = k;
+						}
 					}
-				}
 
-				int otherLightIndex = intersections[neighborIntersectionIndex].roadIndexToLight(neighborParallelRoadIndex);
-				int otherLightTime = currentLightTimes[neighborIntersectionIndex][otherLightIndex];
-				intersections[i].roadDensity[j] += getCarsPassing(otherLightTime);
+					int otherLightIndex = intersections[neighborIntersectionIndex].roadIndexToLight(neighborParallelRoadIndex);
+					int otherLightTime = currentLightTimes[neighborIntersectionIndex][otherLightIndex];
+					intersections[i].roadDensity[j] += getCarsPassing(otherLightTime) * SETTINGS.densityToCarRatio * 
+														float(neighborIntersection->roadsThatIntersect[neighborParallelRoadIndex]->mainLanes) /
+														(thisRoad->travelTime * SETTINGS.travelTimeDensityMultiplier);
+				}
+				else {
+					cout << "No neighbor intersection found";
+				}
 			}
 		}
 	}
 
 	vector<vector<int>> lightOptimization() {
 		struct Node {
+			Map* parent;
 			vector<Intersection> intersections;
 			vector<vector<int>> lightTimes;
-			int potentialFlow = 0;
+			float potentialFlow = 0;
 			int currentIntersection = 0;
 
-			Node(vector<Intersection> intersections, vector<vector<int>> lightTimes, int currentIntersection) {
+			Node(Map* parent, vector<Intersection> intersections, vector<vector<int>> lightTimes, int currentIntersection) {
+				this->parent = parent;
 				this->intersections = intersections;
 				this->lightTimes = lightTimes;
 				this->currentIntersection = currentIntersection;
-				updatePotentialFlow();
+				this-> potentialFlow = findPotentialFlow();
 			}
 
-			void updatePotentialFlow() {
-				potentialFlow = findPotentialFlow();
-			}
-
-			int findPotentialFlow() {
+			float findPotentialFlow() {
 				// use traffic density to find sum of all cars passing through all intersections over time 't'
-				int density = 0;
+				float density = 0;
 				for (int i = 0; i < currentIntersection; i++) {
 					for (int j = 0; j < lightTimes[i].size(); j++) {
-						density += getCarsPassing(lightTimes[i][j]);
+						density += getCarsPassing(lightTimes[i][j]) * intersections[i].roadsThatIntersect[j]->mainLanes;
 					}
 				}
 
-				// TODO:
 				// for lights not yet determined, use ideal flow
 				// assume rest are in the ideal state
-				// ideal be if 100% of cars were incoming from one way and 100% outgoing another way
-				// as if 100% of cars incoming and outgoing the same 2 directions, with max light time on those, and min light time on the others
+				// since cars passing should accout for cars speeding up before they can move,
+				// ideal would be if each light were max time and there was an exact amount of cars to move through the light for max light time
 				for (int i = currentIntersection; i < intersections.size() ; i++) {
 					for (int j = 0; j < lightTimes[i].size(); j++) {
-						density += getCarsPassing(lightTimes[i][j]);
+						density += getCarsPassing(SETTINGS.maximumLightTime) * intersections[i].roadsThatIntersect[j]->mainLanes;
 					}
 				}
-				return density;
+				return density * SETTINGS.densityToCarRatio;
 			}
 		};
 		struct LessThanByPromising {
@@ -229,15 +254,14 @@ public:
 		priority_queue<Node, vector<Node>, LessThanByPromising> pq;
 
 		vector<vector<int>> initialLightTimes;
-		float averageLightTime = (minimumLightTime + maximumLightTime) / 2;
+		float averageLightTime = (SETTINGS.minimumLightTime + SETTINGS.maximumLightTime) / 2;
 		for (int i = 0; i < intersections.size(); i++) {
-			// TODO: bound by min and max (set as average?)
-			// change times to float?
+			// set as average
 			vector<int> singleLightTime(intersections[i].togetherRoads.size(), averageLightTime); /*number of light times as connected roads*/
 			initialLightTimes.push_back(singleLightTime);
 		}
 
-		Node initial(intersections, initialLightTimes, 0);
+		Node initial(this, intersections, initialLightTimes, 0);
 		pq.push(initial);
 		int totalNodes = 1;
 
@@ -251,24 +275,19 @@ public:
 					pq.pop();
 
 					// add more nodes to pq
-					// for each light at the current intersection, try adding 5 to it
-					// TODO: multiple light changes in the same Node
-					// right now each node only changes 1 light
-
 					// ensures it hasn't passed max number of intersections
 					if (currentNode.currentIntersection < currentNode.lightTimes.size()) {
 						int lightCount = currentNode.lightTimes[currentNode.currentIntersection].size();
 						vector<int> changesSet(lightCount, 0);
 						tempSolutions.clear();
-						getLightChanges(changesSet, 0);
-						//backtracking
+						getLightChanges(changesSet, 0); //backtracking  // results explained at that function
 						cout << "Size of thing: " << tempSolutions.size() << endl;
 						for (int i = 0; i < tempSolutions.size(); i++) {
 							vector<vector<int>> newLightTimes = currentNode.lightTimes;
 							for (int j = 0; j < tempSolutions[i].size(); j++) {
 								newLightTimes[currentNode.currentIntersection][j] += tempSolutions[i][j];
 							}
-							Node newNode(intersections, newLightTimes, currentNode.currentIntersection + 1);
+							Node newNode(this, intersections, newLightTimes, currentNode.currentIntersection + 1);
 							pq.push(newNode);
 							totalNodes++;
 						}
@@ -284,14 +303,16 @@ public:
 					}
 				}
 
-				cout << "Optimization iteration ran using Node Count: " << totalNodes << endl;
+				if (DEBUG_MODE) {
+					cout << "Optimization iteration ran using Node Count: " << totalNodes << endl;
+				}
 
 				// spread density
 				doSpreadDensity(resultLightTimes);
 
 				// effectively a clear
 				pq = priority_queue<Node, vector<Node>, LessThanByPromising>();
-				Node nextInitial(intersections, resultLightTimes, 0);
+				Node nextInitial(this, intersections, resultLightTimes, 0);
 				pq.push(initial);
 				totalNodes = 1;
 			}
@@ -303,20 +324,23 @@ public:
 
 void main() {
 	Map map;
-	map.setMin(15);
-	map.setMax(30);
+	SETTINGS.minimumLightTime = 15;
+	SETTINGS.maximumLightTime = 30;
 
-	Road roadA1 = Road("A", 5);
-	Road roadA2 = Road("A", 5);
-	Road roadB1 = Road("B", 5);
-	Road roadB2 = Road("B", 5);
+	Road roadA1 = Road("A", "1", 5, 1);
+	Road roadA2 = Road("A", "2", 5, 1);
+	Road roadB1 = Road("B", "1", 5, 1);
+	Road roadB2 = Road("B", "2", 5, 1);
 
 	vector<Road*> roadList  = { &roadA1, &roadA2, &roadB1, &roadB2 };
-	vector<int> densityList = { 1, 1, 1, 1 };
+	vector<float> densityList = { 1, 1, 1, 1 };
 
 	Intersection intersectionAB = Intersection("A x B", roadList, densityList);
 
-
+	roadA1.setEndPoint(intersectionAB);
+	roadA2.setEndPoint(intersectionAB);
+	roadB1.setEndPoint(intersectionAB);
+	roadB2.setEndPoint(intersectionAB);
 
 	map.intersections.push_back(intersectionAB);
 
